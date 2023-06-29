22df076bd5e750240e025e886372ee6108ac0f3d
VicGar21 <victorgarp15@hotmail.com>
2023-06-28 18:31:19 -0600

    Crea funcion para eleccion del jugador

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 /* Se crea función para que la computadora realice su elección */
 function getComputerChoice(){
+    
     /* Crea variable para obtener número aleatorio entre 0 y 2 incluidos */
     let numeroAleatorio = Math.floor(Math.random() * (2 - 0 + 1)+ 0);
 
