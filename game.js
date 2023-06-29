a2b39fa4c176658095274ae24f08f8059963df18
VicGar21 <victorgarp15@hotmail.com>
2023-06-28 18:34:18 -0600

    Preguntar a jugador su eleccion

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 /* Se crea función para que la computadora realice su elección */
 function getComputerChoice(){
-    
+
     /* Crea variable para obtener número aleatorio entre 0 y 2 incluidos */
     let numeroAleatorio = Math.floor(Math.random() * (2 - 0 + 1)+ 0);
 
@@ -13,4 +13,11 @@ function getComputerChoice(){
         case 2:
             return "Tijeras"
     }
+}
+
+/* Se crea función para que el jugador elija su arma */
+function playerSelection(){
+
+    /* Crea variable para eleccion de jugador */
+    let jugador = prompt("Elija cual es el arma que utilizará: \n a) Piedra \n b) Papel \n c) Tijeras")
 }
